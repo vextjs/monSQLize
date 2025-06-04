b1afd51618fb2d3a4f2c496f6abf4f712b94a196
gali11 <rockyshi1993@gmail.com>
2025-06-04 17:59:21 +0800

    添加增加修改集合方法

--- example/mongo.js
+++ example/mongo.js
@@ -1,11 +1,12 @@
 const SQLize = require('../lib/index');
 (async ()=>{
     const db = await new SQLize({
-        db: 'mongo',
+        dbType: 'mongodb',                // 连接类型
+        databaseName :'my_database',           // 数据库名称
         config: {
-            uri: 'mongodb://localhost:27017/test'
+            uri: 'mongodb://localhost:27017'
         }
     }).connect();
 
-    console.log(await db('demo').findOne())
+    console.log(await db('demo').create('test'))
 })();
