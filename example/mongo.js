a3437af16ecd155b13932e19ff7e449fd03224c9
rocky <rockyshi1993@gmail.com>
2025-08-21 20:44:41 +0800

    封装logger

--- example/mongo.js
+++ example/mongo.js
@@ -1,8 +1,8 @@
-const SQLize = require('../lib/index');
+const MonSQLize = require('../lib/index');
 (async ()=>{
-    const db = await new SQLize({
-        dbType: 'mongodb',                // 连接类型
-        databaseName :'my_database',           // 数据库名称
+    const db = await new MonSQLize({
+        type: 'mongodb',                // 连接类型
+        databaseName :'test',           // 数据库名称
         config: {
             uri: 'mongodb://localhost:27017'
         }
